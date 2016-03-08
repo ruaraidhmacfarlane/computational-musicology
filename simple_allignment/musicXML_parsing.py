e996845a11c8d490e26f2ea5191d42b4a13c06d8
Ruaraidh Macfarlane <ruaraidh2@gmail.com>
2016-03-08 16:22:44 +0000

    added python components

---
+++ simple_allignment/musicXML_parsing.py
@@ -0,0 +1,29 @@
+import music21, sys
+
+
+class MusicXMLParsing:
+
+	parsed_score = None
+	rhythm_hash = []
+	parsons_hash = []
+	gapped_score_rhythm = []
+	length = 0
+	gapped_bar_num = 0
+
+	def __init__(self, path):
+		self.parsed_score = music21.converter.parse(path)
+		self.rhythm_hash = music21.omr.correctors.ScoreCorrector(self.parsed_score).singleParts[0].hashedNotes
+		self.length = len(self.rhythm_hash)
+	# def parsons_code(self):		
+
+	# Only to work for hashed rhythms for now, when parsons is included this will create a gap in that array too
+	def create_gap(self, bar):
+		if bar <= len(self.rhythm_hash):
+			self.gapped_score_rhythm = self.rhythm_hash
+			self.gapped_score_rhythm.pop(bar-1)
+			self.gapped_bar_num = bar
+		else:
+			sys.exit("Error: Cannot create a gap, bar is out of range of music")
+
+
+
---
+++ simple_allignment/simple_allignments.py
@@ -0,0 +1,98 @@
+import copy, musicXML_parsing
+
+
+def simple_allignment(gapped_piece, comparison_piece):
+	comparison_arr_size = gapped_piece.length + comparison_piece.length
+	# Just using it for rhythms so far
+	extended_pieces = extend(gapped_piece.gapped_score_rhythm, comparison_piece.rhythm_hash)
+	gapped_piece_extended = extended_pieces[0]
+	comparison_piece_extended = extended_pieces[1]
+
+	gapped_piece_index = 0
+	replacing_bar_arr = []
+	min_edits_arr = []
+
+	for i in range(comparison_arr_size):
+		tuple_arr = fill_arr(i, gapped_piece_extended, comparison_piece_extended)
+		gapped_piece_extended = tuple_arr[0]
+		comparison_piece_extended = tuple_arr[1]
+
+		temp_gapped_piece_extended = copy.deepcopy(gapped_piece_extended)
+		temp_comparison_piece_extended = copy.deepcopy(comparison_piece_extended)
+
+		if i % 2 == 1:
+			gapped_piece_index += 1
+
+		bar_num = gapped_piece_index + removed_bar_no - 1
+		removed_bar = temp_gapped_piece_extended.pop(bar_num)
+		replacing_bar_arr.append(temp_comparison_piece_extended.pop(bar_num))
+
+		min_edits_arr.append(edit_distance(temp_gapped_piece_extended, temp_comparison_piece_extended))
+
+	returned_bar = find_gap_bar(replacing_bar_arr, min_edits_arr)
+	min_edit_distance = min(min_edits_arr)
+
+	return returned_bar, min_edit_distance
+
+def find_gap_bar(replacing_bars, edit_distances):
+	return replacing_bars[edit_distances.index(min(edit_distances))]
+
+def extend(x, y):
+	a = ['    '] * len(y)
+	b = ['    '] * len(x)
+	x_extended = x + a
+	y_extended = b + y
+	return(x_extended, y_extended)
+
+def fill_arr(index, x, y):
+	if index % 2 == 0:
+		swap = x.pop()
+		x = [swap] + x
+	else:
+		swap = y.pop(0)
+		y = y + [swap]
+
+	return (x, y)
+
+def substCost(x, y):
+    if x == y: 
+        return 0
+    else: 
+        return 2
+    
+def insertCost(x):
+    return 1
+
+def deleteCost(x):
+    return 1
+
+def edit_distance(x, y):
+    n = len(x)
+    m = len(y)
+    
+    distance = [[0 for i in range(m+1)] for j in range(n+1)]
+
+    for i in range(1,n+1):
+        distance[i][0] = distance[i-1][0] + insertCost(x[i-1])
+
+    for j in range(1,m+1):
+        distance[0][j] = distance[0][j-1] + deleteCost(y[j-1])
+
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            distance[i][j] = min(distance[i-1][j] + 1,
+                                 distance[i][j-1] + 1,
+                                 distance[i-1][j-1] + substCost(y[j-1],x[i-1]))
+    return distance[n][m]
+
+def main():
+	a = musicXML_parsing.MusicXMLParsing('../musicXML/tests/rhythm-test.xml')
+	b = musicXML_parsing.MusicXMLParsing('../musicXML/tests/rhythm-test.xml')
+
+	a.create_gap(3)
+
+	# replaced_bar = simple_allignment(a, b, 1)
+	# print replaced_bar
+	return
+
+main()
