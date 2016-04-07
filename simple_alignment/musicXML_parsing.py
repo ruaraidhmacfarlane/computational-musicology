86c44ceaf51f99f75a6b8fbb37c3da4d661f571c
Ruaraidh Macfarlane <ruaraidh2@gmail.com>
2016-04-07 17:40:57 +0100

    improved simple alignment. Extends now only the shorter piece of music.

--- simple_alignment/musicXML_parsing.py
+++ simple_alignment/musicXML_parsing.py
@@ -1,82 +1,86 @@
-import music21, sys, copy
+import music21
+import sys
+
 
 class MusicXMLParsing:
+    parsed_score = None
+    parsed_work = None
+    rhythm_hash = []
+    # pitch_hash = []
+    parsons_code = []
+    length = 0
+    gapped_bar_num = 0
+    # Hard coded for now until gaps in bars get bigger
+    GAP_LENGTH = 1
 
-	parsed_score = None
-	parsed_work = None
-	rhythm_hash = []
-	pitch_hash = []
-	parsons_code = []
-	length = 0
-	gapped_bar_num = 0
-	# Hard coded for now until gaps in bars get bigger
-	GAP_LENGTH = 1
+    def __init__(self, path):
+        if (self.is_kern(path)):
+            path = music21.converter.parse(path).write('musicxml')
+        self.parsed_score = music21.converter.parse(path)
+        # self.parsed_score.show()
+        # self.parsed_work = music21.parseWork(path)
+        self.rhythm_hash = music21.omr.correctors.ScoreCorrector(self.parsed_score).singleParts[0].hashedNotes
+        self.length = len(self.rhythm_hash)
+        # self.pitch_hash = self._hash_pitches()
+        self.parsons_code = self._parsons_code()
 
-	def __init__(self, path):
-		if(self.is_kern(path)):
-			path = music21.converter.parse(path).write('musicxml')
-		self.parsed_score = music21.converter.parse(path)
-		# self.parsed_score.show()
-		# self.parsed_work = music21.parseWork(path)
-		self.rhythm_hash = music21.omr.correctors.ScoreCorrector(self.parsed_score).singleParts[0].hashedNotes
-		self.length = len(self.rhythm_hash)
-		# self.pitch_hash = self._hash_pitches()
-		self.parsons_code = self._parsons_code()
+    def is_kern(self, path_name):
+        split_path = path_name.split('.')
+        if split_path[-1] == 'krn':
+            return True
+        else:
+            return False
 
+    def create_gap(self, bar):
+        if bar <= len(self.rhythm_hash):
+            self.gapped_bar_num = bar
+            self.rhythm_hash[bar - 1] = '    '
+            self.parsons_code[bar - 1] = '    '
+        else:
+            sys.exit("Error: Cannot create a gap, bar is out of range of music")
 
-	def is_kern(self, path_name):
-		split_path = path_name.split('.')
-		if split_path[-1] == 'krn':
-			return True
-		else:
-			return False
-
-	def create_gap(self, bar):
-		if bar <= len(self.rhythm_hash):
-			self.gapped_bar_num = bar
-		else:
-			sys.exit("Error: Cannot create a gap, bar is out of range of music")
-
-	"""
+    """
 	u = "up," if the note is higher than the previous note
 	d = "down," if the note is lower than the previous note
 	r = "repeat," if the note is the same pitch as the previous note
 	* = first tone as reference
 	"""
-	def _parsons_code(self):
-		contour_arr = []
-		measure_map = self.parsed_score.parts[0].measureOffsetMap()
-		measures = sorted(measure_map)
-		for m in measures:
-			bar_string = ""
-			# maybe first bars are rests
-			if len(measure_map[m][0].notes.pitches) != 0:
-				index = len(contour_arr)
-				for pitch in measure_map[m][0].notes.pitches:
-					if index == 0 and len(bar_string) == 0:
-						last_pitch = pitch
-						bar_string += '*'
-					else:
-						bar_string += self._compare_pitch(last_pitch, pitch)
-						last_pitch = pitch
-				contour_arr.append(bar_string)
-		return contour_arr
 
-	def _compare_pitch(self, last_pitch, pitch):
-		pitch_list = ['C', 'C#', 'D-', 'D', 'D#', 'E-', 'E', 'F-', 'E#' , 'F', 'F#', 'G-', 'G', 'G#', 'A-', 'A', 'A#', 'B-', 'B', 'C-', 'B#']
-		if last_pitch.nameWithOctave == pitch.nameWithOctave:
-			return 'r'
-		if last_pitch.octave != pitch.octave:
-			if last_pitch.octave < pitch.octave:
-				return 'u'
-			else:
-				return 'd'
-		else:
-			last_pitch_index = pitch_list.index(last_pitch.name)
-			curr_pitch_index = pitch_list.index(pitch.name)
-			if last_pitch_index < curr_pitch_index:
-				return 'u'
-			else:
-				return 'd'
+    def _parsons_code(self):
+        contour_arr = []
+        measure_map = self.parsed_score.parts[0].measureOffsetMap()
+        measures = sorted(measure_map)
+        for m in measures:
+            bar_string = ""
+            # maybe first bars are rests
+            if len(measure_map[m][0].notes.pitches) != 0:
+                index = len(contour_arr)
+                for pitch in measure_map[m][0].notes.pitches:
+                    if index == 0 and len(bar_string) == 0:
+                        last_pitch = pitch
+                        bar_string += '*'
+                    else:
+                        bar_string += self._compare_pitch(last_pitch, pitch)
+                        last_pitch = pitch
+                contour_arr.append(bar_string)
+        return contour_arr
 
-	# def _convert_bars(self, contour_arr):
+    def _compare_pitch(self, last_pitch, pitch):
+        pitch_list = ['C', 'C#', 'D-', 'D', 'D#', 'E-', 'E', 'F-', 'E#', 'F', 'F#', 'G-', 'G', 'G#', 'A-', 'A', 'A#',
+                      'B-', 'B', 'C-', 'B#']
+        if last_pitch.nameWithOctave == pitch.nameWithOctave:
+            return 'r'
+        if last_pitch.octave != pitch.octave:
+            if last_pitch.octave < pitch.octave:
+                return 'u'
+            else:
+                return 'd'
+        else:
+            last_pitch_index = pitch_list.index(last_pitch.name)
+            curr_pitch_index = pitch_list.index(pitch.name)
+            if last_pitch_index < curr_pitch_index:
+                return 'u'
+            else:
+                return 'd'
+
+            # def _convert_bars(self, contour_arr):
--- simple_alignment/simple_alignments.py
+++ simple_alignment/simple_alignments.py
@@ -1,230 +1,261 @@
-import music21, copy, musicXML_parsing, os
+import music21
+import copy
+import musicXML_parsing
+import os
+
 
 class SimpleAlignment:
+    # parson || rhythm
+    attr = ''
+    gapped_parse = None
+    comparison_parse = None
+    gapped_piece = []
+    comparison_piece = []
+    extended_length = 0
+    min_edit_alignments = []
+    min_edit_obj = None
+    right_shift = 0
+    left_shift = 0
 
-	# parson || rhythm
-	attr = ''
-	gapped_parse = None
-	comparison_parse = None
-	gapped_extended_piece = []
-	comparison_extended_piece = []
-	extended_length = 0
-	min_edit_alignments = []
-	min_edit_obj = None
-	right_shift = 0
-	left_shift = 0
-
-	def __init__(self, gapped, comparison, attr):
-		self.attr = attr
-		self.gapped_parse = gapped
-		self.comparison_parse = comparison
-		self.extended_length = gapped.length + comparison.length
-		self.align()
-		self.get_min_edit_obj()
-
-	def get_min_edit_obj(self):
-		minimum_edit = self.min_edit_alignments[0].edit_distance
-		min_obj = None
-		for i in self.min_edit_alignments:
-			if i.edit_distance < minimum_edit:
-				minimum_edit = i.edit_distance
-				min_obj = i
-		self.min_edit_obj = min_obj
-
-	def align(self):
-		self.extend()
-		self.create_gap()
-		print self.gapped_extended_piece
-		print self.comparison_extended_piece
-		for i in range(self.extended_length):
-			self.min_edit_alignments.append(EditDistance(i, self))
-			self.adjust(i)
-			# print self.gapped_extended_piece
-			# print self.comparison_extended_piece
+    def __init__(self, gapped, comparison, feat):
+        self.attr = feat
+        self.gapped_parse = gapped
+        self.comparison_parse = comparison
+        if self.attr == 'parson':
+            self.gapped_piece = copy.deepcopy(self.gapped_parse).parsons_code
+            self.comparison_piece = copy.deepcopy(self.comparison_parse).parsons_code
+        elif self.attr == 'rhythm':
+            self.gapped_piece = copy.deepcopy(self.gapped_parse).rhythm_hash
+            self.comparison_piece = copy.deepcopy(self.comparison_parse).rhythm_hash
+        # self.extended_length = gapped.length + comparison.length
+        self.align()
+        # self.get_min_edit_obj()
 
 
-	def extend(self):
-		a = [''] * self.gapped_parse.length
-		b = [''] * self.comparison_parse.length
-		self.left_shift = len(b)
-		if self.attr == 'parson':
-			self.gapped_extended_piece = copy.deepcopy(self.gapped_parse).parsons_code + a
-			self.comparison_extended_piece = b + copy.deepcopy(self.comparison_parse).parsons_code
-		elif self.attr == 'rhythm':
-			self.gapped_extended_piece = copy.deepcopy(self.gapped_parse).rhythm_hash + a
-			self.comparison_extended_piece = b + copy.deepcopy(self.comparison_parse).rhythm_hash
+    def get_min_edit_obj(self):
+        minimum_edit = self.min_edit_alignments[0].edit_distance
+        min_obj = None
+        for i in self.min_edit_alignments:
+            if i.edit_distance < minimum_edit:
+                minimum_edit = i.edit_distance
+                min_obj = i
+        self.min_edit_obj = min_obj
 
-	def create_gap(self):
-		self.gapped_extended_piece.pop(self.gapped_parse.gapped_bar_num - 1)
-		self.gapped_extended_piece.append('')
 
-	def adjust(self, adjust_index):
-		if adjust_index % 2 == 0:
-			swap = self.gapped_extended_piece.pop()
-			self.gapped_extended_piece = [swap] + self.gapped_extended_piece
-			self.right_shift += 1
-		else:
-			swap = self.comparison_extended_piece.pop(0)
-			self.comparison_extended_piece = self.comparison_extended_piece + [swap]
-			self.left_shift -= 1
+    def align(self):
+        # self.extend()
+        # self.create_gap()
+        # print self.gapped_extended_piece
+        # print self.comparison_extended_piece
+
+        gapped_length = len(self.gapped_piece)
+        compare_length = len(self.comparison_piece)
+
+        if gapped_length == compare_length:
+            print self.gapped_piece
+            print self.comparison_piece
+        elif gapped_length > compare_length:
+            extended_piece = self.extend(self.comparison_piece, self.gapped_piece)
+            print self.gapped_piece
+            print extended_piece
+        elif gapped_length < compare_length:
+            extended_piece = self.extend(self.gapped_piece, self.comparison_piece)
+            print extended_piece
+            print self.comparison_piece
+            # self.shift(compare_length)
+
+            # for i in range(self.extended_length):
+            #     self.min_edit_alignments.append(EditDistance(i, self))
+            #     self.adjust(i)
+            # print self.gapped_extended_piece
+            # print self.comparison_extended_piece
+
+    def extend(self, shorter, longer):
+        difference = len(longer) - len(shorter)
+        extended_arr = ['    '] *  difference
+        return shorter + extended_arr
+
+    # def shift(self, ):
+        # def extend(self):
+        #     a = ['    '] * self.gapped_parse.length
+        #     b = ['    '] * self.comparison_parse.length
+        #     self.left_shift = len(b)
+        #     if self.attr == 'parson':
+        #         self.gapped_extended_piece = copy.deepcopy(self.gapped_parse).parsons_code + a
+        #         self.comparison_extended_piece = b + copy.deepcopy(self.comparison_parse).parsons_code
+        #     elif self.attr == 'rhythm':
+        #         self.gapped_extended_piece = copy.deepcopy(self.gapped_parse).rhythm_hash + a
+        #         self.comparison_extended_piece = b + copy.deepcopy(self.comparison_parse).rhythm_hash
+        #
+        # def create_gap(self):
+        #     self.gapped_extended_piece.pop(self.gapped_parse.gapped_bar_num - 1)
+        #     self.gapped_extended_piece.append('    ')
+    def adjust(self, adjust_index):
+        if adjust_index % 2 == 0:
+            swap = self.gapped_extended_piece.pop()
+            self.gapped_extended_piece = [swap] + self.gapped_extended_piece
+            self.right_shift += 1
+        else:
+            swap = self.comparison_extended_piece.pop(0)
+            self.comparison_extended_piece = self.comparison_extended_piece + [swap]
+            self.left_shift -= 1
+
 
 class EditDistance:
+    edit_distance = -1
+    replacing_arr_index = 0
+    actual_replaced_bar_num = 0
+    replaced_bar = '    '
+    adjust_index = -1
+    comparison_obj = None
 
-	edit_distance = -1
-	replacing_arr_index = 0
-	actual_replaced_bar_num = 0
-	replaced_bar = '    '
-	adjust_index = -1
-	comparison_obj = None
+    def __init__(self, index, comparison_obj):
+        self.adjust_index = index
+        self.comparison_obj = copy.deepcopy(comparison_obj)
+        if self.adjust_index % 2 == 0:
+            self.replacing_arr_index = comparison_obj.right_shift + comparison_obj.gapped_parse.gapped_bar_num - 1
+        else:
+            self.replacing_arr_index = comparison_obj.gapped_parse.gapped_bar_num + comparison_obj.right_shift - 1
+        temp_extended_copy = copy.deepcopy(comparison_obj.comparison_extended_piece)
+        temp_replaced_bar = temp_extended_copy.pop(self.replacing_arr_index)
+        self.get_edit_distance(comparison_obj.gapped_extended_piece, temp_extended_copy)
+        if temp_replaced_bar != '    ':
+            self.actual_replaced_bar_num = self._get_replaced_bar_num()
+            self.replaced_bar = temp_replaced_bar
 
-	def __init__(self, index, comparison_obj):
-		self.adjust_index = index
-		self.comparison_obj = copy.deepcopy(comparison_obj)
-		if self.adjust_index % 2 == 0:
-			self.replacing_arr_index = comparison_obj.right_shift + comparison_obj.gapped_parse.gapped_bar_num - 1
-		else:
-			self.replacing_arr_index = comparison_obj.gapped_parse.gapped_bar_num + comparison_obj.right_shift - 1
-		temp_extended_copy = copy.deepcopy(comparison_obj.comparison_extended_piece)
-		temp_replaced_bar = temp_extended_copy.pop(self.replacing_arr_index)
-		self.get_edit_distance(comparison_obj.gapped_extended_piece, temp_extended_copy)
-		if temp_replaced_bar != '    ':
-			self.actual_replaced_bar_num = self._get_replaced_bar_num()
-			self.replaced_bar = temp_replaced_bar
+    def _get_replaced_bar_num(self):
+        number = self.replacing_arr_index - self.comparison_obj.left_shift + 1
+        return number
 
-	def _get_replaced_bar_num(self):
-		number = self.replacing_arr_index - self.comparison_obj.left_shift + 1
-		return number
+    def get_edit_distance(self, gapped, temp_comparison):
+        print gapped
+        print temp_comparison
+        trimmed = self._trim(gapped, temp_comparison)
+        gapped = trimmed[0]
+        temp_comparison = trimmed[1]
+        n = len(gapped)
+        m = len(temp_comparison)
 
-	def get_edit_distance(self, gapped, temp_comparison):	
-		trimmed = self._trim(gapped, temp_comparison)
-		gapped = trimmed[0]
-		temp_comparison = trimmed[1]
-		n = len(gapped)
-		m = len(temp_comparison)
+        distance = [[0 for i in range(m + 1)] for j in range(n + 1)]
 
-		distance = [[0 for i in range(m+1)] for j in range(n+1)]
+        for i in range(1, n + 1):
+            distance[i][0] = distance[i - 1][0] + self._insert_cost(gapped[i - 1])
 
-		for i in range(1,n+1):
-			distance[i][0] = distance[i-1][0] + self._insert_cost(gapped[i-1])
+        for j in range(1, m + 1):
+            distance[0][j] = distance[0][j - 1] + self._delete_cost(temp_comparison[j - 1])
 
-		for j in range(1,m+1):
-			distance[0][j] = distance[0][j-1] + self._delete_cost(temp_comparison[j-1])
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                distance[i][j] = min(distance[i - 1][j] + 1,
+                                     distance[i][j - 1] + 1,
+                                     distance[i - 1][j - 1] + self._subst_cost(temp_comparison[j - 1], gapped[i - 1]))
 
-		for i in range(1,n+1):
-			for j in range(1,m+1):
-				distance[i][j] = min(distance[i-1][j] + 1,
-				distance[i][j-1] + 1,
-				distance[i-1][j-1] + self._subst_cost(temp_comparison[j-1],gapped[i-1]))
+        self.edit_distance = distance[n][m]
 
-		self.edit_distance = distance[n][m]
+    def _trim(self, x, y):
+        first_x = -1
+        first_y = -1
+        last_x = -1
+        last_y = -1
+        first_hit_x = False
+        first_hit_y = False
 
-	def _trim(self, x, y):
-		first_x = -1
-		first_y = -1
-		last_x = -1
-		last_y = -1
-		first_hit_x = False
-		first_hit_y = False
+        for i in range(len(x)):
+            if first_hit_x == False and x[i] != '   ':
+                first_hit_x = True
+                first_x = i
+            if first_hit_x == True and x[i] == '    ':
+                last_x = i
+                break
 
-		for i in range(len(x)):
-			if first_hit_x == False and x[i] != '   ':
-				first_hit_x = True
-				first_x = i
-			if first_hit_x == True and x[i] == '    ':
-				last_x = i
-				break
+        for j in range(len(y)):
+            if first_hit_y == False and y[j] != '    ':
+                first_hit_y = True
+                first_y = j
+            if first_hit_y == True and y[j] == '    ':
+                last_y = j
+                break
 
-		for j in range(len(y)):
-			if first_hit_y == False and y[j] != '    ':
-				first_hit_y = True
-				first_y = j
-			if first_hit_y == True and y[j] == '    ':
-				last_y = j
-				break
+        if first_x == -1:
+            first_x = 0
+        if first_y == -1:
+            first_y = 0
+        if last_x == -1:
+            last_x = len(x)
+        if last_y == -1:
+            last_y = len(y)
 
-		if first_x == -1:
-			first_x = 0
-		if first_y == -1:
-			first_y = 0
-		if last_x == -1:
-			last_x = len(x)
-		if last_y == -1:
-			last_y = len(y)
+        first = min(first_x, first_y)
+        last = max(last_x, last_y)
+        return (x[first:last], y[first:last])
 
-		first = min(first_x, first_y)
-		last = max(last_x, last_y)
-		return(x[first:last], y[first:last])
+    def _subst_cost(self, x, y):
+        if x == y:
+            return 0
+        else:
+            return 2
 
-	def _subst_cost(self, x, y):
-		if x == y:
-			return 0
-		else:
-			return 2
+    def _insert_cost(self, x):
+        return 1
 
-	def _insert_cost(self, x):
-		return 1
+    def _delete_cost(self, x):
+        return 1
 
-	def _delete_cost(self, x):
-		return 1
 
 class Corpus:
+    old_corpus_file = ""
+    new_corpus_file = ""
+    database = []
 
-	old_corpus_file = ""
-	new_corpus_file = ""
-	database = []
+    def __init__(self, old_file, new_file):
+        self.old_corpus_file = old_file
+        self.new_corpus_file = new_file
 
-	def __init__(self, old_file, new_file):
-		self.old_corpus_file = old_file
-		self.new_corpus_file = new_file
+    def fill_database(self):
+        with open(self.new_corpus_file) as corpus:
+            for path in corpus:
+                path = path.rstrip()
+                self.database.append(musicXML_parsing.MusicXMLParsing(path))
 
-	def  fill_database(self):
-		with open(self.new_corpus_file) as corpus:
-			for path in corpus:
-				path = path.rstrip()
-				self.database.append(musicXML_parsing.MusicXMLParsing(path))
+    def list_dir(self):
+        old_corpus = open(self.old_corpus_file, "w")
+        for file in os.listdir("../musicXML/palestrina"):
+            if file.endswith(".krn") or file.endswith(".xml"):
+                old_corpus.write("../musicXML/palestrina/" + file + "\n")
+        old_corpus.close()
 
+    def clean(self):
+        new_corpus = open(self.new_corpus_file, "w")
+        line_num = 0
+        with open(self.old_corpus_file) as corpus:
+            for path in corpus:
+                line_num += 1
+                path = path.rstrip()
+                print 'parsing line ', line_num
+                try:
+                    musicXML_parsing.MusicXMLParsing(path)
+                    new_corpus.write(path + "\n")
+                except AttributeError:
+                    print '%s is a bad score', path
+                except music21.exceptions21.StreamException:
+                    print '%s is a not 4/4', path
+                print 'Parsed', path
 
-	def list_dir(self):
-		old_corpus = open(self.old_corpus_file, "w")
-		for file in os.listdir("../musicXML/palestrina"):
-			if file.endswith(".krn") or file.endswith(".xml"):
-				old_corpus.write("../musicXML/palestrina/" + file + "\n")
-		old_corpus.close()
+        new_corpus.close()
 
-	def clean(self):
-		new_corpus = open(self.new_corpus_file, "w")
-		line_num = 0
-		with open(self.old_corpus_file) as corpus:
-			for path in corpus:
-				line_num += 1
-				path = path.rstrip()
-				print 'parsing line ', line_num
-				try:
-					musicXML_parsing.MusicXMLParsing(path)
-					new_corpus.write(path + "\n")
-				except AttributeError: 
-					print '%s is a bad score', path
-				except music21.exceptions21.StreamException:
-					print '%s is a not 4/4', path
-				print 'Parsed', path
-
-		new_corpus.close()
 
 def main():
-	
-	# ground_truth_x = musicXML_parsing.MusicXMLParsing('../musicXML/palestrina/Missa-Petra-sancta_Benedictus_Palestrina-Giovanni-Pierluigi-da_file1.krn')
+    # database = Corpus("path-list.txt", "parsable-path-list.txt")
+    # database.fill_database()
 
-	
-	# database = []
+    ground_truth = musicXML_parsing.MusicXMLParsing('../musicXML/tests/rhythm-test.xml')
+    compare = musicXML_parsing.MusicXMLParsing('../musicXML/tests/test1.xml')
+    ground_truth.create_gap(3)
+    align = SimpleAlignment(ground_truth, compare, 'rhythm')
 
-	database = Corpus("path-list.txt", "parsable-path-list.txt")
-	database.fill_database()
 
-	# ground_truth_x.create_gap(3)
-
-	# align = SimpleAlignment(pitch_x, pitch_y, 'rhythm')
-	# print 'edit distance: ', align.min_edit_obj.edit_distance
-	# print 'replaced bar: ', align.min_edit_obj.replaced_bar
-	# print 'replaced bar number: ', align.min_edit_obj.actual_replaced_bar_num
+# print 'edit distance: ', align.min_edit_obj.edit_distance
+# print 'replaced bar: ', align.min_edit_obj.replaced_bar
+# print 'replaced bar number: ', align.min_edit_obj.actual_replaced_bar_num
 
 main()
